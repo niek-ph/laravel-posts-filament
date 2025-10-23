9add1160b51fb16ae72293c776df86e00b13dd66
niek-ph <64198878+niek-ph@users.noreply.github.com>
2025-10-23 12:42:56 +0000

    Fix styling

--- src/Filament/Resources/Posts/Pages/ViewPost.php
+++ src/Filament/Resources/Posts/Pages/ViewPost.php
@@ -14,7 +14,8 @@ class ViewPost extends ViewRecord
 
     protected function getHeaderActions(): array
     {
-        $isPublished = !is_null($this->record->published_at);
+        $isPublished = ! is_null($this->record->published_at);
+
         return [
             EditAction::make(),
 
@@ -34,5 +35,4 @@ class ViewPost extends ViewRecord
                 }),
         ];
     }
-
 }
